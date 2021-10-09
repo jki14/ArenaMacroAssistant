9a31d7770931d326a095f486b0bb2684e3a18396
jki14 <jki14wz@gmail.com>
2021-10-09 23:29:29 +0900

    Support both Opponent and Team updates
    Add the Team updates to lock the party slot of the teammate.

--- ArenaMacroAssistant/ArenaMacroAssistant.lua
+++ ArenaMacroAssistant/ArenaMacroAssistant.lua
@@ -1,6 +1,8 @@
+--[[
 function chatmsg(text)
     DEFAULT_CHAT_FRAME:AddMessage('|cFFFF7C0A[ArenaMacroAssistant]' .. text)
 end
+]]--
 
 function macroEdit(name, id, icons, iconFallback, body)
     local body = string.gsub(body, '%%d', tostring(id))
@@ -8,7 +10,7 @@ function macroEdit(name, id, icons, iconFallback, body)
     return EditMacro(name, nil, icon, body)
 end
 
-function macroUpdate(name, classId, icons, iconFallback, body)
+function macroUpdateOpponent(name, classId, icons, iconFallback, body)
     for i = 1, 5 do
         local cid = select(3, UnitClass(string.format('arena%d',i)))
         if cid and cid == classId then
@@ -36,17 +38,54 @@ function macroUpdate(name, classId, icons, iconFallback, body)
     return false
 end
 
-function run()
-    -- chatmsg(' on update')
-    macroUpdate('!faerieRogue', 4,
-                {133252, 133242, 133269, 133259}, 133272,
-                '#showtooltips\n/cast [@arena%d]Faerie Fire(Rank 1)\n/use Zapthrottle Mote Extractor')
-    macroUpdate('!pounceRogue', 4,
-                {133251, 133241, 133268, 133258}, 133271,
-                '#showtooltips Discombobulator Ray\n/cast [@arena%d]Pounce\n/use Discombobulator Ray')
+function macroUpdateTeam(name, classId, icons, iconFallback, body)
+    for i = 1, 4 do
+        local cid = select(3, UnitClass(string.format('party%d',i)))
+        if cid and cid == classId then
+            local function handler()
+                if not macroEdit(name, i, icons, iconFallback, body) then
+                    C_Timer.After(0.2, handler)
+                end
+            end
+            C_Timer.After(0.2, handler)
+            return true
+        end
+    end
+    local function handler()
+        if not macroEdit(name, 1, icons, iconFallback, body) then
+            C_Timer.After(0.2, handler)
+        end
+    end
+    C_Timer.After(0.2, handler)
+    return false
 end
 
-local arenaMacroAssistant = CreateFrame('Frame')
-arenaMacroAssistant:RegisterEvent('ARENA_OPPONENT_UPDATE')
-arenaMacroAssistant:SetScript('OnEvent', run)
--- chatmsg(' initialized')
+function runOpponent()
+    macroUpdateOpponent('!faerieRogue', 4,
+                        {133252, 133242, 133269, 133259}, 133272,
+                        '#showtooltips\n/cast [@arena%d]Faerie Fire(Rank 1)\n/use Zapthrottle Mote Extractor')
+    macroUpdateOpponent('!pounceRogue', 4,
+                        {133251, 133241, 133268, 133258}, 133271,
+                        '#showtooltips Discombobulator Ray\n/cast [@arena%d]Pounce\n/use Discombobulator Ray')
+end
+
+local arenaMacroAssistantOpponent = CreateFrame('Frame')
+arenaMacroAssistantOpponent:RegisterEvent('ARENA_OPPONENT_UPDATE')
+arenaMacroAssistantOpponent:SetScript('OnEvent', runOpponent)
+
+function runTeam()
+    macroUpdateTeam('!abolishPriest', 5, {134082, 134113, 134079}, 134110,
+                    '#showtooltips Abolish Poison\n/cast [@player,mod:alt][@focus,help,mod:ctrl][@party%d,mod:ctrl][@target,help]Abolish Poison;Soothe Animal')
+    local curseList = {2, 7, 5, 9}
+    for i = 1, #curseList do
+        if macroUpdateTeam('!removePaladin', curseList[i], {134082, 134113, 134079}, 134110,
+                           '#showtooltips Remove Curse\n/cast [@player,mod:alt][@focus,help,mod:ctrl][@party%d,mod:ctrl][@target,help][@targettarget,help]Remove Curse') then
+            break
+        end
+    end
+end
+
+local arenaMacroAssistantTeam = CreateFrame('Frame')
+arenaMacroAssistantTeam:RegisterEvent('ARENA_TEAM_UPDATE')
+arenaMacroAssistantTeam:RegisterEvent('GROUP_ROSTER_UPDATE')
+arenaMacroAssistantTeam:SetScript('OnEvent', runTeam)
